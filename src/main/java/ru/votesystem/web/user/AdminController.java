1aca3e00ae65eb95e68adba0257a438bc06af99e
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2023-12-06 20:45:46 +0700

    add caching

--- src/main/java/ru/votesystem/web/user/AdminController.java
+++ src/main/java/ru/votesystem/web/user/AdminController.java
@@ -1,6 +1,8 @@
 package ru.votesystem.web.user;
 
 import jakarta.validation.Valid;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,29 +17,34 @@ public class AdminController extends AbstractUserController {
     public static final String REST_URL = "/rest/admin/users";
 
     @GetMapping
+    @Cacheable("users")
     public List<User> getAll() {
         log.info("getAll()");
         return repository.findAll();
     }
 
     @GetMapping("/{id}")
+    @CacheEvict(value = "users", allEntries = true)
     public User get(@PathVariable int id) {
         return super.get(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @CacheEvict(value = "users", allEntries = true)
     public User create(@Valid @RequestBody User user) {
         return super.create(user);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CacheEvict(value = "users", allEntries = true)
     public void delete(@PathVariable int id) {
         super.delete(id);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CacheEvict(value = "users", allEntries = true)
     public void update(@Valid @RequestBody User user, @PathVariable int id) {
         super.update(user, id);
     }
