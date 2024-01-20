d212bce95a03a28f1fb612adde856e4651ac0fa6
alekseialenkin <124128504+alekseialenkin@users.noreply.github.com>
2024-01-20 12:57:10 +0700

    fixed SecurityConfig

--- src/main/java/ru/votesystem/config/SecurityConfig.java
+++ src/main/java/ru/votesystem/config/SecurityConfig.java
@@ -59,9 +59,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/rest/**").authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/rest/admin/**").hasRole(Role.ADMIN.name())
                         .requestMatchers(HttpMethod.POST, "/rest/restaurants", "/rest/restaurants/{restaurantId}/dishes").hasRole(Role.ADMIN.name())
-                        .requestMatchers(HttpMethod.DELETE, "/rest/*/restaurants", "/rest/restaurants/{restaurantId}/dishes").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE, "/rest/restaurants", "/rest/restaurants/{restaurantId}/dishes").hasRole(Role.ADMIN.name())
                         .requestMatchers(HttpMethod.PUT, "/rest/restaurants", "/rest/restaurants/{restaurantId}/dishes").hasRole(Role.ADMIN.name())
                         .requestMatchers(HttpMethod.POST, "/rest/profile").anonymous()
                         .requestMatchers("/**").authenticated()
